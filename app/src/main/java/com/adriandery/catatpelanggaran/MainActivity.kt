ce672ec8d08803d1b47105eaeec3de9974d0c744
adriandk <adriandaniel1803@gmail.com>
2020-10-26 13:44:12 +0700

    Design

--- app/src/main/java/com/adriandery/catatpelanggaran/MainActivity.kt
+++ app/src/main/java/com/adriandery/catatpelanggaran/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.adriandery.catatpelanggaran
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }, 2500)
+
     }
 }
